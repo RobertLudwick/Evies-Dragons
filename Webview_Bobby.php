7cf45299a80ce1d149568af40494f36de53edbdd
Bobby Ludwick <rludwick@drew.edu>
2017-11-14 02:41:00 -0500

    Added a character creator page

--- Webview_Bobby.php
+++ Webview_Bobby.php
@@ -1,43 +1,96 @@
 <html>
 <head><title>D&D Character Creator</title></head>
 <body>
+	<?php
+		$sqlHost = 'localhost';
+		$sqlUser = 'root';
+		$sqlPass = 'root';
+		$dbName = 'Evies';
+		$mysqli = new mysqli($sqlHost, $sqlUser, $sqlPass, $dbName);
+		if (mysqli_connect_error()) {
+		    die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
+		}
+	?>
+  
   <form method="post" action="">
-    <input type="text" name="something" value="" />
+	<p>Name	
+	<input type="name" name="something" value="" />
+	</p>
+    <p>Races
+    <select name="Races">
+    	<option value="">Race</option>
+    	<?php
+			$result = $mysqli->query("SELECT race_name FROM races;")
+				or trigger_error($db->error);
+			while($row = $result->fetch_array()) {
+     			echo "<option value='".$row[0]."'>'".$row[0]."'</option>";
+     		}
+		?>
+	</select>
+	</p>
+	<p>Variants
+    <select name="Variants">
+    	<option value="">Variant</option>
+    	<?php
+			$result = $mysqli->query("SELECT variant_name FROM variants;")
+				or trigger_error($db->error);
+//			if () {
+			while($row = $result->fetch_array()) {
+     			echo "<option value='".$row[0]."'>'".$row[0]."'</option>";
+     		}
+//			}
+		?>
+	</select>
+	</p>
+	<p>Classes
+	<select name="Classes">
+		<option value="">Class</option>
+	    <?php
+			$result = $mysqli->query("SELECT class_name FROM class;")
+	    		or trigger_error($db->error);
+// 	    	while($row = $result->fetch_array()) {
+// 		 		echo "<option value='".$row[0]."'>'".$row[0]."'</option>";
+// 	    	}
+		?>
+	</select>
+	</p>
+	<p>Backgrounds
+	<select name="Backgrounds">
+		<option value="">Backgrounds</option>
+	    <?php
+			$result = $mysqli->query("SELECT background_name FROM Backgrounds;")
+	    		or trigger_error($db->error);
+// 	    	while($row = $result->fetch_array()) {
+// 		 		echo "<option value='".$row[0]."'>'".$row[0]."'</option>";
+// 	    	}
+		?>
+	</select>
+	</p>
+	<p>Strength	
+	<input type="Strength" name="something" value="" />
+	</p>
+	<p>Dexterity	
+	<input type="Dexterity" name="something" value="" />
+	</p>
+	<p>Constitution	
+	<input type="Constitution" name="something" value="" />
+	</p>
+	<p>Intelligence	
+	<input type="Intelligence" name="something" value="" />
+	</p>
+	<p>Wisdom	
+	<input type="Wisdom" name="something" value="" />
+	</p>
+	<p>Charisma	
+	<input type="Charisma" name="something" value="" />
+	</p>
     <input type="submit" name="submit" />
   </form>
 
 <?php
-$sqlHost = 'localhost';
-$sqlUser = 'root';
-$sqlPass = 'root';
-$dbName = 'Evies';
-$mysqli = new mysqli($sqlHost, $sqlUser, $sqlPass, $dbName);
-if (mysqli_connect_error()) {
-    die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
-}
-// echo 'Connected successfully.';
-$result = $mysqli->query("SELECT * FROM Skills;")
-    or trigger_error($db->error);
-//var_dump($result);?>
-<TABLE>
-<TR>
-<TH>skill_name</TH>
-<TH>skill_descript</TH>
-</TR>
-<?php
-$array = array('skill_name', 'skill_descript');
-while($row = $result->fetch_array()) {
-    // Less DRY ways to display for testing
-    //echo var_dump($row);
-    //echo "working";
-    //echo $row['skill_name'].": ".$row['skill_descript'];
-    //echo "<BR>";
-    echo "<TR>";
-    foreach($array as $field) {
-        echo "<TD>".$row[$field]."</TD>";
-    }
-    echo "</TR>";
-}
+
+
+
 $db->close();
  ?>
 </TABLE>
