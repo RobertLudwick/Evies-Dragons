f6e8b53349167d27c02f06aae4b945211e1da1c0
Ida Behreini <idabehreini@Idas-MacBook-Air.local>
2017-11-14 03:07:24 -0500

    Homepage & CSS edits

--- Webview_Ida.php
+++ Webview_Ida.php
@@ -1,11 +1,53 @@
 <html>
-<head><title>D&D Character Creator</title></head>
-<body>
-  <form method="post" action="">
-    <input type="text" name="something" value="" />
-    <input type="submit" name="submit" />
-  </form>
+<head><title>D&D Character Creator</title>
+<style>
+h1 {
+    text-align: center;
+    color: white;
 
+    }
+    body {
+        background-color: black;
+    }
+ .button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
+
+.button1 {
+    background-color: black; 
+    color: white; 
+    border: 2px solid #4CAF50;
+}
+
+.button1:hover {
+    background-color: #4CAF50;
+    color: white;
+
+}
+
+</style>
+    </head>
+<body>
+<h1>Welcome to the Dungeons and Dragons Character Builder!</h1>
+<div>
+</div>
+<div class='myDiv'>
+    <a href = "ViewCharacters.php"<button class ="button button1"style='margin-right:500px'>View Existing Characters</button></a>
+    <a href="Webview_Bobby.php"<button class ="button button1">Create New Character</button></a>
+</div>
+<center><img src = "dragon.jpg" alt = "dragon"
+width = "500" height = "300"></center>
 <?php
 $sqlHost = 'localhost';
 $sqlUser = 'root';
@@ -16,28 +58,7 @@ if (mysqli_connect_error()) {
     die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
 }
 // echo 'Connected successfully.';
-$result = $mysqli->query("SELECT * FROM Skills;")
-    or trigger_error($db->error);
-//var_dump($result);?>
-<TABLE>
-<TR>
-<TH>skill_name</TH>
-<TH>skill_descript</TH>
-</TR>
-<?php
-$array = array('skill_name', 'skill_descript');
-while($row = $result->fetch_array()) {
-    // Less DRY ways to display for testing
-    //echo var_dump($row);
-    //echo "working";
-    //echo $row['skill_name'].": ".$row['skill_descript'];
-    //echo "<BR>";
-    echo "<TR>";
-    foreach($array as $field) {
-        echo "<TD>".$row[$field]."</TD>";
-    }
-    echo "</TR>";
-}
+
 $db->close();
  ?>
 </TABLE>
