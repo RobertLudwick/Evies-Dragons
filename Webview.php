2fc2e5c42e4846616397095dd7d76e22af1f4852
Andrew Rupkey <arupkey@drew.edu>
2017-11-14 00:58:10 -0500

    Page name changed

--- Webview.php
+++ Webview.php
@@ -1,5 +1,5 @@
 <html>
-<head><title>Get Movie Info</title></head>
+<head><title>D&D Character Creator</title></head>
 <body>
   <form method="post" action="">
     <input type="text" name="something" value="" />
@@ -7,21 +7,15 @@
   </form>
 
 <?php
-
-
 $sqlHost = 'localhost';
 $sqlUser = 'root';
 $sqlPass = 'root';
 $dbName = 'Evies';
-
 $mysqli = new mysqli($sqlHost, $sqlUser, $sqlPass, $dbName);
-
 if (mysqli_connect_error()) {
     die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
 }
-
 // echo 'Connected successfully.';
-
 $result = $mysqli->query("SELECT * FROM Skills;")
     or trigger_error($db->error);
 //var_dump($result);?>
@@ -32,7 +26,6 @@ $result = $mysqli->query("SELECT * FROM Skills;")
 </TR>
 <?php
 $array = array('skill_name', 'skill_descript');
-
 while($row = $result->fetch_array()) {
     // Less DRY ways to display for testing
     //echo var_dump($row);
