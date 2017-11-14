8a81baebefdb2e31c7bdc64b2d266a9564dd6a1e
Bobby Ludwick <rludwick@drew.edu>
2017-11-14 00:46:25 -0500

    deleted unnecessary code

--- Webview.php
+++ Webview.php
@@ -8,57 +8,6 @@
 
 <?php
 
-// if(isset($_POST['submit'])) {
-//   echo 'Query: ', htmlspecialchars($_POST['something']);
-// }
-
-
-// $user = 'root';
-// $password = 'root';
-// $db = 'Evies';
-// $socket = 'localhost:/Applications/MAMP/tmp/mysql/mysql.sock';
-// 
-// $link = mysql_connect(
-//    $socket, 
-//    $user, 
-//    $password
-// );
-// $db_selected = mysql_select_db(
-//    $db, 
-//    $link
-// );
-
-// $user = 'root';
-// $password = 'root';
-// $db = 'Evies';
-// $host = 'localhost';
-// $port = 8889;
-// 
-// $link = mysql_connect(
-//    "$host:$port", 
-//    $user, 
-//    $password
-// );
-// $db_selected = mysql_select_db(
-//    $db, 
-//    $link
-// );
-
-// $user = 'root';
-// $password = 'root';
-// $db = 'Evies';
-// $host = 'localhost';
-// $port = 8889;
-// 
-// $link = mysqli_init();
-// $success = mysqli_real_connect(
-//    $link, 
-//    $host, 
-//    $user, 
-//    $password, 
-//    $db,
-//    $port
-// );
 
 $sqlHost = 'localhost';
 $sqlUser = 'root';
@@ -71,7 +20,7 @@ if (mysqli_connect_error()) {
     die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
 }
 
-echo 'Connected successfully.';
+// echo 'Connected successfully.';
 
 $result = $mysqli->query("SELECT * FROM Skills;")
     or trigger_error($db->error);
@@ -87,8 +36,8 @@ $array = array('skill_name', 'skill_descript');
 while($row = $result->fetch_array()) {
     // Less DRY ways to display for testing
     //echo var_dump($row);
-    echo "working";
-    echo $row['skill_name'].": ".$row['skill_descript'];
+    //echo "working";
+    //echo $row['skill_name'].": ".$row['skill_descript'];
     //echo "<BR>";
     echo "<TR>";
     foreach($array as $field) {
