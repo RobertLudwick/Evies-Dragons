1bedef3880975991511500a2e34d511dd59caaf0
Ida Behreini <idabehreini@Idas-MacBook-Air.local>
2017-12-11 16:07:05 -0500

    Added a login page which checks user input with database entries.

---
+++ login.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+<?php
+	$sqlHost = 'localhost';
+	$sqlUser = 'root';
+	$sqlPass = 'root';
+	$dbName = 'Evies';
+    $db_table = 'user';
+		$mysqli = new mysqli($sqlHost, $sqlUser, $sqlPass, $dbName);
+		if (mysqli_connect_error()) {
+		    die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
+		}
+
+$username = $_POST['Username'];
+$password = $_POST['Password'];
+$count = 0;
+$query = $mysqli->query("SELECT username, password FROM user WHERE username = '".$username."' AND  password = '".$password."'");
+
+  //$query = $mysqli->query("SELECT username, password FROM user;");
+  while($row = $query->fetch_array()) {
+    if ($username == $row[0] && $password = $row[1]){ 
+      $count = $count + 1;
+    }
+  }
+  if ($count != 0) {
+    echo "Login successful. Continue to homepage.";
+  }
+  if ($count == 0) {
+    echo "Incorrect username and/or password. Try again or register.";
+  }
+
+	?>
+
+</body>
+</html>
---
+++ signin.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+  <form action="login.php" method="post">
+Username: <input type="text" name="Username"><br>
+Password: <input type="password" name="Password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
