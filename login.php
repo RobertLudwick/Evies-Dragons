2f8933c22210993901ee0f7ace664b0fa2afd619
Ida Behreini <idabehreini@Idas-MacBook-Air.local>
2017-12-12 06:22:54 -0500

    Fixed a bug in login system

--- login.php
+++ login.php
@@ -1,21 +1,19 @@
 <html>
 <body>
 <?php
-	$sqlHost = 'localhost';
-	$sqlUser = 'root';
-	$sqlPass = 'root';
-	$dbName = 'Evies';
+  $sqlHost = 'localhost';
+  $sqlUser = 'root';
+  $sqlPass = 'root';
+  $dbName = 'Evies';
     $db_table = 'user';
-		$mysqli = new mysqli($sqlHost, $sqlUser, $sqlPass, $dbName);
-		if (mysqli_connect_error()) {
-		    die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
-		}
-
+    $mysqli = new mysqli($sqlHost, $sqlUser, $sqlPass, $dbName);
+    if (mysqli_connect_error()) {
+        die('Connect Error ('.mysqli_connect_errno().') '.mysqli_connect_error());
+    }
 $username = $_POST['Username'];
 $password = $_POST['Password'];
 $count = 0;
 $query = $mysqli->query("SELECT username, password FROM user WHERE username = '".$username."' AND  password = '".$password."'");
-
   //$query = $mysqli->query("SELECT username, password FROM user;");
   while($row = $query->fetch_array()) {
     if ($username == $row[0] && $password = $row[1]){ 
@@ -23,14 +21,13 @@ $query = $mysqli->query("SELECT username, password FROM user WHERE username = '"
     }
   }
   if ($count != 0) {
-    echo "<script>window.top.location='http://localhost:8888/home.php'</script>";
+    echo "<script>window.top.location='http://localhost:8888/home.php?" . $username . "'</script>";
     //echo "Login successful. Continue to homepage.";
   }
   if ($count == 0) {
     echo "Incorrect username and/or password. Try again or register.";
   }
-
-	?>
+  ?>
 
 </body>
 </html>
