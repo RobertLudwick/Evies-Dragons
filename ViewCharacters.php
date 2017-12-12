aee20a016d67b103baa5c7388d9b792042b02577
Andrew Rupkey <arupkey@drew.edu>
2017-12-12 04:43:42 -0500

    Individual Character view updated

--- ViewCharacters.php
+++ ViewCharacters.php
@@ -1,7 +1,7 @@
 <html>
 <head><title>D&D Characters</title></head>
 <body>
-	<h1>Created Characters</h1>
+
 <?php
 $sqlHost = 'localhost';
 $sqlUser = 'root';
@@ -14,6 +14,7 @@ if (mysqli_connect_error()) {
 $cname=$_SERVER['QUERY_STRING'];
 //echo $cname;
 $cname2 = str_replace("%20", " ", $cname);
+echo '<h1>' . $cname2 . '</h1>';
 //echo $cname2;
 //$test = "SELECT * FROM players WHERE char_name = '" . $cname . "';";
 //echo $test;
@@ -52,14 +53,140 @@ $array = array('char_id', 'char_name', 'player_name', 'gender', 'race', 'variant
 while($row = $result->fetch_array()) {
     echo "<TR>";
     foreach($array as $field) {
+        $race = $row[4];
+        $variant = $row[5];
+        $class = $row[6];
+        $clevel = $row[17];
         echo "<TD>".$row[$field]."</TD>";
     }
     echo "</TR>";
 }
 
  ?>
-
 </TABLE>
+
+<TABLE>
+    <TR>
+        <TH>Race Name</TH>
+        <TH>Variant</TH>
+        <TH>Size</TH>
+        <TH>Speed</TH>
+        <TH>Strength</TH>
+        <TH>Dexterity</TH>
+        <TH>Constitution</TH>
+        <TH>Intelligence</TH>
+        <TH>Wisdom</TH>
+        <TH>Charisma</TH>
+        <TH>Proficiencies</TH>
+        <TH>Skills</TH>
+        <TH>Languages</TH>
+        <TH>Resistances</TH>
+        <TH>Advantages</TH>
+    </TR>
+    <?php
+    $result2 = $mysqli->query("SELECT * FROM races WHERE race_name = '" . $race . "';")
+    or trigger_error($db->error);
+    $array2 = array('race_name', 'variant', 'size', 'speed', 'Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma', 'Proficiencies', 'Skills', 'languages', 'resistances', 'advantages');
+    while($row2 = $result2->fetch_array()) {
+        echo "<TR>";
+        foreach ($array2 as $field2) {
+            echo "<TD>" . $row2[$field2] . "</TD>";
+        }
+        echo "</TR>";
+    }
+    ?>
+</TABLE>
+
+<TABLE>
+    <TR>
+        <TH>Class Name</TH>
+        <TH>HP</TH>
+        <TH>Hit Dice</TH>
+        <TH>Proficiencies</TH>
+        <TH>Choosen Skills</TH>
+        <TH>Skills</TH>
+        <TH>Equipment</TH>
+    </TR>
+    <?php
+    $result3 = $mysqli->query("SELECT * FROM classes WHERE class_name = '" . $class . "';")
+    or trigger_error($db->error);
+    $array3 = array('class_name', 'HP', 'hitdie', 'Proficiencies', 'choose_skills', 'Skills', 'Equipment');
+    while($row3 = $result3->fetch_array()) {
+        echo "<TR>";
+        foreach ($array3 as $field3) {
+            echo "<TD>" . $row3[$field3] . "</TD>";
+        }
+        echo "</TR>";
+    }
+    ?>
+</TABLE>
+
+<TABLE>
+    <TR>
+        <TH>Race Name</TH>
+        <TH>Variant Name</TH>
+        <TH>Strength</TH>
+        <TH>Dexterity</TH>
+        <TH>Constitution</TH>
+        <TH>Intelligence</TH>
+        <TH>Wisdom</TH>
+        <TH>Charisma</TH>
+        <TH>Proficiencies</TH>
+        <TH>Skills</TH>
+        <TH>Languages</TH>
+        <TH>Resistances</TH>
+        <TH>Advantages</TH>
+    </TR>
+    <?php
+
+    $result4 = $mysqli->query("SELECT * FROM variants WHERE variant_name = '" . $variant . "';")
+    or trigger_error($db->error);
+    $array4 = array('race_name', 'variant_name', 'Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma', 'Proficiencies', 'Skills', 'languages', 'resistances', 'advantages');
+    while($row4 = $result4->fetch_array()) {
+        echo "<TR>";
+        foreach ($array4 as $field4) {
+            echo "<TD>" . $row4[$field4] . "</TD>";
+        }
+        echo "</TR>";
+    }
+    ?>
+</TABLE>
+
+<TABLE>
+    <TR>
+        <TH>Class Name</TH>
+        <TH>Level</TH>
+        <TH>Proficiency Bonus</TH>
+        <TH>Bonus</TH>
+        <TH>Bonus Points</TH>
+        <TH>Features</TH>
+        <TH>Cantrips</TH>
+        <TH>Spells</TH>
+        <TH>1st</TH>
+        <TH>2nd</TH>
+        <TH>3rd</TH>
+        <TH>4rd</TH>
+        <TH>5th</TH>
+        <TH>6th</TH>
+        <TH>7th</TH>
+        <TH>8th</TH>
+        <TH>9th</TH>
+        <TH>Bonus Damage</TH>
+    </TR>
+    <?php
+    $result5 = $mysqli->query("SELECT * FROM levels WHERE class_name = '" . $class . "' AND level = '" . $clevel . "';")
+    or trigger_error($db->error);
+    $array5 = array('class_name', 'Level', 'Prof_bonus', 'Bonus', 'Bonus_points', 'Features', 'Cantrips', 'Spells', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '9th', 'bonus_damage');
+    while($row5 = $result5->fetch_array()) {
+        echo "<TR>";
+        foreach ($array5 as $field5) {
+            echo "<TD>" . $row5[$field5] . "</TD>";
+        }
+        echo "</TR>";
+    }
+    ?>
+</TABLE>
+
 <a href="Webview_Bobby.php"> Create a Character</a>
 </body>
 <?php
